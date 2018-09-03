cc30b6c25130bd4374238681bad3df4340373038
Chungchen "John" Ran <chungchen.ran@sjsu.edu>
2018-09-03 14:04:26 -0700

    Test machine types 1 and 2.

--- lab1/gumball-java-typical/Main.java
+++ lab1/gumball-java-typical/Main.java
@@ -4,25 +4,26 @@ public class Main {
 
 	public static void main(String[] args) {
 		GumballMachine gumballMachine1 = new GumballMachine(1, 5);
-		GumballMachine gumballMachine2 = new GumballMachine(1, 5);
-		GumballMachine gumballMachine3 = new GumballMachine(1, 5);
+		GumballMachine gumballMachine2 = new GumballMachine(2, 5);
+		//GumballMachine gumballMachine3 = new GumballMachine(1, 5);
 
 		System.out.println(gumballMachine1);
 		System.out.println("Type: " + gumballMachine1.showType());
-
-		gumballMachine1.insertQuarter( 25 );
+                
+		gumballMachine1.insertCoin(25);
 		gumballMachine1.turnCrank();
 
 		System.out.println(gumballMachine2);
 		System.out.println("Type: " + gumballMachine2.showType());
-
-		gumballMachine2.insertQuarter( 25 );
+                
+		gumballMachine2.insertCoin(25);
+		gumballMachine2.insertCoin(25);
 		gumballMachine2.turnCrank();
 		
-		System.out.println(gumballMachine3);
-		System.out.println("Type: " + gumballMachine3.showType());
+		//System.out.println(gumballMachine3);
+		//System.out.println("Type: " + gumballMachine3.showType());
 		
-		gumballMachine3.insertQuarter( 25 );
-		gumballMachine3.turnCrank();
+		//gumballMachine3.insertCoin(25);
+		//gumballMachine3.turnCrank();
 	}
 }
