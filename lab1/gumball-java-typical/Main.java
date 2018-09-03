3264748b4e4175f79ca9ae398379788f1ab79d78
Chungchen "John" Ran <chungchen.ran@sjsu.edu>
2018-09-03 09:27:38 -0700

    Show the type of each gumball machine instance.

--- lab1/gumball-java-typical/Main.java
+++ lab1/gumball-java-typical/Main.java
@@ -3,21 +3,24 @@
 public class Main {
 
 	public static void main(String[] args) {
-		GumballMachine gumballMachine1 = new GumballMachine(5);
-		GumballMachine gumballMachine2 = new GumballMachine(5);
-		GumballMachine gumballMachine3 = new GumballMachine(5);
+		GumballMachine gumballMachine1 = new GumballMachine(1, 5);
+		GumballMachine gumballMachine2 = new GumballMachine(1, 5);
+		GumballMachine gumballMachine3 = new GumballMachine(1, 5);
 
 		System.out.println(gumballMachine1);
+		System.out.println("Type: " + gumballMachine1.showType());
 
 		gumballMachine1.insertQuarter( 25 );
 		gumballMachine1.turnCrank();
 
 		System.out.println(gumballMachine2);
+		System.out.println("Type: " + gumballMachine2.showType());
 
 		gumballMachine2.insertQuarter( 25 );
 		gumballMachine2.turnCrank();
 		
 		System.out.println(gumballMachine3);
+		System.out.println("Type: " + gumballMachine3.showType());
 		
 		gumballMachine3.insertQuarter( 25 );
 		gumballMachine3.turnCrank();
