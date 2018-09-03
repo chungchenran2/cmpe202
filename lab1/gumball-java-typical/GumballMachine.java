d38b4b281004651a08611b47fbb7ede2ffa02ce4
Chungchen "John" Ran <chungchen.ran@sjsu.edu>
2018-09-03 14:02:54 -0700

    Implement the support for machine types 1 and 2

--- lab1/gumball-java-typical/GumballMachine.java
+++ lab1/gumball-java-typical/GumballMachine.java
@@ -4,6 +4,7 @@ public class GumballMachine
     public int machine_type;
     private int num_gumballs;
     private boolean has_quarter;
+    private boolean has_2_quarters;
     private int gumball_cost;
     private int total_coin_amount;
 
@@ -13,10 +14,11 @@ public class GumballMachine
         this.machine_type = type;
         this.num_gumballs = size;
         this.has_quarter = false;
+        this.has_2_quarters = false;
         
-        if (this.machine_type==1)
+        if (this.machine_type == 1)
             this.gumball_cost = 25;
-        else if ((this.machine_type==2) || (this.machine_type==3))
+        else if ((this.machine_type == 2) || (this.machine_type == 3))
             this.gumball_cost = 50;
         else
         {
@@ -32,36 +34,59 @@ public class GumballMachine
         return this.machine_type;
     }
 
-    public void insertQuarter(int coin)
+    public void insertCoin(int coin)
     {
-        if (coin==25)
+        if (this.machine_type == 1)
         {
-            this.has_quarter = true;
-            this.total_coin_amount += 25;
+            if (coin == 25)
+                this.has_quarter = true;
+            else 
+                this.has_quarter = false;
         }
-        else 
-            this.has_quarter = false;
+        else if (this.machine_type == 2)
+        {
+            if (coin == 25)
+                this.total_coin_amount += 25;
+            
+            if (this.total_coin_amount == 50)
+                this.has_2_quarters = true;
+        }
     }
     
     public void turnCrank()
     {
-        if (this.has_quarter)
+        if (this.machine_type == 1)
         {
-            if ((this.num_gumballs>0) && (this.total_coin_amount==this.gumball_cost))
+            if (this.has_quarter)
             {
-                this.num_gumballs--;
-                this.has_quarter = false;
-                this.total_coin_amount = 0;
-                System.out.println("Thanks for your quarter.  Gumball Ejected!");
+                if (this.num_gumballs > 0)
+                {
+                    this.num_gumballs--;
+                    this.has_quarter = false;
+                    System.out.println("Thanks for your quarter. Gumball Ejected!");
+                }
+                else
+                    System.out.println("No More Gumballs! Sorry, can't return your quarter.");
+            }
+            else 
+                System.out.println("Please insert a quarter.");
+        }
+        else if (this.machine_type == 2)
+        {
+            if (this.has_2_quarters)
+            {
+                if (this.num_gumballs > 0)
+                {
+                    this.num_gumballs--;
+                    this.has_2_quarters = false;
+                    this.total_coin_amount = 0;
+                    System.out.println("Thanks for your 2 quarters. Gumball Ejected!");
+                }
+                else
+                    System.out.println("No More Gumballs! Sorry, can't return your quarters.");
             }
             else
-            {
-                System.out.println("No More Gumballs!  Sorry, can't return your quarter.");
-            }
+                System.out.println("Please insert only 2 quarters.");
         }
-        else 
-        {
-            System.out.println("Please insert a quarter");
-        }        
     }
 }
