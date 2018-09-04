773cc78aa2850a3158e4d139d687a99ce38423cc
Chungchen "John" Ran <chungchen.ran@sjsu.edu>
2018-09-04 09:47:11 -0700

    Remove the cost variable and modify coin limit.

--- lab1/gumball-java-typical/GumballMachine.java
+++ lab1/gumball-java-typical/GumballMachine.java
@@ -4,29 +4,21 @@ public class GumballMachine
     public int machine_type;
     private int num_gumballs;
     private boolean has_quarter;
-    private boolean has_2_quarters;
-    private int gumball_cost;
+    private boolean has_2nd_quarter;
     private int total_coin_amount;
 
     public GumballMachine(int type, int size)
     {
         // initialise instance variables
         this.machine_type = type;
+        if (this.machine_type > 3)
+        {
+            System.out.println("Invalid type, setting machine to type 1 as default.");
+            this.machine_type = 1;
+        }
         this.num_gumballs = size;
         this.has_quarter = false;
-        this.has_2_quarters = false;
-        
-        if (this.machine_type == 1)
-            this.gumball_cost = 25;
-        else if ((this.machine_type == 2) || (this.machine_type == 3))
-            this.gumball_cost = 50;
-        else
-        {
-            System.out.println("Invalid type, setting to type 1 as default.");
-            this.machine_type = 1;
-            this.gumball_cost = 25;
-        }
-        
+        this.has_2nd_quarter = false;
         this.total_coin_amount = 0;
     }
     
@@ -40,22 +32,34 @@ public class GumballMachine
         if (this.machine_type == 1)
         {
             if (coin == 25)
-                this.has_quarter = true;
+            {
+                if (!this.has_quarter)
+                    this.has_quarter = true;
+                else
+                    System.out.println("Cannot insert more than 1 quarter.");
+            }
             else 
                 this.has_quarter = false;
         }
         else if (this.machine_type == 2)
         {
             if (coin == 25)
-                this.total_coin_amount += 25;
-            
-            if (this.total_coin_amount == 50)
-                this.has_2_quarters = true;
+            {
+                if (!this.has_quarter)
+                    this.has_quarter = true;
+                else if (this.has_quarter && !this.has_2nd_quarter)
+                    this.has_2nd_quarter = true;
+                else
+                    System.out.println("Cannot insert more than 2 quarters.");
+            }
+            else
+            {
+                this.has_quarter = false;
+                this.has_2nd_quarter = false;
+            }
         }
         else if (this.machine_type == 3)
-        {
             this.total_coin_amount += coin;
-        }
     }
     
     public void turnCrank()
@@ -78,13 +82,13 @@ public class GumballMachine
         }
         else if (this.machine_type == 2)
         {
-            if (this.has_2_quarters)
+            if (this.has_quarter && this.has_2nd_quarter)
             {
                 if (this.num_gumballs > 0)
                 {
                     this.num_gumballs--;
-                    this.has_2_quarters = false;
-                    this.total_coin_amount = 0;
+                    this.has_quarter = false;
+                    this.has_2nd_quarter = false;
                     System.out.println("Thanks for your 2 quarters. Gumball Ejected!");
                 }
                 else
@@ -95,13 +99,14 @@ public class GumballMachine
         }
         else if (this.machine_type == 3)
         {
-            if (this.total_coin_amount == 50)
+            if (this.total_coin_amount >= 50)
             {
                 if (this.num_gumballs > 0)
                 {
                     this.num_gumballs--;
-                    this.total_coin_amount = 0;
+                    this.total_coin_amount -= 50;
                     System.out.println("Thanks for your coins. Gumball Ejected!");
+                    System.out.println(this.total_coin_amount + " cents left in machine.");
                 }
                 else
                     System.out.println("No More Gumballs! Sorry, can't return your coins.");
