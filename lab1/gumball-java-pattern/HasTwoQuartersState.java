00b2fff590444fc65af40cce3f0cf16c34637e96
Chungchen "John" Ran <chungchen.ran@gmail.com>
2018-09-07 10:47:54 -0700

    Add state for two quarters in second model.

---
+++ lab1/gumball-java-pattern/HasTwoQuartersState.java
@@ -0,0 +1,31 @@
+
+public class HasTwoQuartersState implements State
+{
+    GumballMachine gumballMachine;
+    
+    public HasTwoQuartersState(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+    
+    public void insertQuarter() {
+        System.out.println("You can't insert another quarter");
+    }
+ 
+    public void ejectQuarter() {
+        System.out.println("Quarter returned");
+        gumballMachine.setState(gumballMachine.getHasQuarterState());
+    }
+ 
+    public void turnCrank() {
+        System.out.println("You turned...");
+        gumballMachine.setState(gumballMachine.getSoldState());
+    }
+
+    public void dispense() {
+        System.out.println("No gumball dispensed");
+    }
+ 
+    public String toString() {
+        return "waiting for turn of crank";
+    }
+}
