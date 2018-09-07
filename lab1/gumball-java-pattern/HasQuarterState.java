d1bb9f1fb85f29028fcb88039425a73f8536d5ea
Chungchen "John" Ran <chungchen.ran@sjsu.edu>
2018-09-07 10:45:55 -0700

    Add cases for second model.

--- lab1/gumball-java-pattern/HasQuarterState.java
+++ lab1/gumball-java-pattern/HasQuarterState.java
@@ -3,31 +3,40 @@
 import java.util.Random;
 
 public class HasQuarterState implements State {
-	GumballMachine gumballMachine;
+    GumballMachine gumballMachine;
  
-	public HasQuarterState(GumballMachine gumballMachine) {
-		this.gumballMachine = gumballMachine;
-	}
+    public HasQuarterState(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
   
-	public void insertQuarter() {
-		System.out.println("You can't insert another quarter");
-	}
+    public void insertQuarter() {
+        if (gumballMachine.getMachineType() == 1)
+            System.out.println("You can't insert another quarter");
+        else if (gumballMachine.getMachineType() == 2)
+        {
+            System.out.println("You inserted a second quarter");
+            gumballMachine.setState(gumballMachine.getHasTwoQuartersState());
+        }
+    }
  
-	public void ejectQuarter() {
-		System.out.println("Quarter returned");
-		gumballMachine.setState(gumballMachine.getNoQuarterState());
-	}
+    public void ejectQuarter() {
+        System.out.println("Quarter returned");
+        gumballMachine.setState(gumballMachine.getNoQuarterState());
+    }
  
-	public void turnCrank() {
-		System.out.println("You turned...");
-		gumballMachine.setState(gumballMachine.getSoldState());
-	}
+    public void turnCrank() {
+        System.out.println("You turned...");
+        if (gumballMachine.getMachineType() == 1)
+            gumballMachine.setState(gumballMachine.getSoldState());
+        else if (gumballMachine.getMachineType() == 2)
+            System.out.println("Still needs another quarter");
+    }
 
     public void dispense() {
         System.out.println("No gumball dispensed");
     }
  
-	public String toString() {
-		return "waiting for turn of crank";
-	}
+    public String toString() {
+        return "waiting for turn of crank";
+    }
 }
