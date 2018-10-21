6d04bf43774a108e12e6eb0d4d8afd090a364339
Chungchen "John" Ran <chungchen.ran@gmail.com>
2018-10-21 16:24:48 -0700

    brute force implementation

--- lab8/inputmask/src/main/java/CreditCardExp.java
+++ lab8/inputmask/src/main/java/CreditCardExp.java
@@ -3,29 +3,32 @@
 public class CreditCardExp implements IDisplayComponent, IKeyEventHandler
 {
 
-	private IKeyEventHandler nextHandler ;
-	private String date = "" ;
+    private IKeyEventHandler nextHandler ;
+    private String date = "" ;
 
     public void setNext( IKeyEventHandler next) {
-    	this.nextHandler = next ;
-    }	
+        this.nextHandler = next ;
+    }   
 
-	public String display() {
-		if ( date.equals("") )
-			return "[MM/YY]" + "  " ;
-		else
-			return "[" + date + "]" + "  " ;
-	}	
+    public String display() {
+        if ( date.equals("") )
+            return "[MM/YY]" + "  " ;
+        else
+            return "[" + date + "]" + "  " ;
+    }   
 
-	public void key(String ch, int cnt) {
-		if ( cnt >= 17 && cnt <= 20  )
-			date += ch ;
-		else if ( nextHandler != null )
-			nextHandler.key(ch, cnt) ;
-	}	
+    public void key(String ch, int cnt) {
+        if ( cnt >= 17 && cnt <= 20  ){
+            date += ch ;
+            if (cnt == 18)
+                date += "/";
+        }
+        else if ( nextHandler != null )
+            nextHandler.key(ch, cnt) ;
+    }   
 
-	public void addSubComponent( IDisplayComponent c ) {
-		return ; // do nothing
-	}
+    public void addSubComponent( IDisplayComponent c ) {
+        return ; // do nothing
+    }
 
 }
--- lab8/inputmask/src/main/java/CreditCardNum.java
+++ lab8/inputmask/src/main/java/CreditCardNum.java
@@ -3,29 +3,40 @@
 public class CreditCardNum implements IDisplayComponent, IKeyEventHandler
 {
 
-	private IKeyEventHandler nextHandler ;
-	private String number = "" ;
+    private IKeyEventHandler nextHandler ;
+    private String number = "" ;
 
     public void setNext( IKeyEventHandler next) {
-    	this.nextHandler = next ;
-    }	
+        this.nextHandler = next ;
+    }   
 
-	public String display() {
-		if ( number.equals("") )
-			return "[4444 4444 4444 4444]" + "  " ;
-		else
-			return "[" + number + "]" + "  " ;
-	}	
+    public String display() {
+        if ( number.equals("") )
+            return "[4444 4444 4444 4444]" + "  " ;
+        else {
+            //return "[" + number + "]" + "  " ;
+            String res = "";
+            //res = "[" + number + "]" + "  " ;
+            res += "[";
+            for (int i = 0; i < number.length(); i++) {
+                res += number.charAt(i);
+                if (i==3 || i==7 || i==11)
+                    res += " ";
+            }
+            res += "]  ";
+            return res;
+        }
+    }   
 
-	public void key(String ch, int cnt) {
-		if ( cnt <= 16 )
-			number += ch ;
-		else if ( nextHandler != null )
-			nextHandler.key(ch, cnt) ;
-	}	
+    public void key(String ch, int cnt) {
+        if ( cnt <= 16 )
+            number += ch ;
+        else if ( nextHandler != null )
+            nextHandler.key(ch, cnt) ;
+    }   
 
-	public void addSubComponent( IDisplayComponent c ) {
-		return ; // do nothing
-	}
+    public void addSubComponent( IDisplayComponent c ) {
+        return ; // do nothing
+    }
 
 }
